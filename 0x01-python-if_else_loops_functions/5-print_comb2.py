801035fa5571847cfe36c8ef27cf973b399f0c19
kimmiriti <miritibriankimathi@gmail.com>
2022-12-01 00:21:39 +0300

    changes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     if i ==99:
-        print("{}" . format(i))
+        print("{}" . format(i), "\n")
     else:
         print("{:02}" .format(i), end=", ")
         
